c3cf2ef824b693d8f2ebb983b1bc662ad0637820
Gauti Nils Bernhardsson <gauti@reontech.com>
2019-08-29 17:35:13 +0000

    About page

--- assets/js/sweater.js
+++ assets/js/sweater.js
@@ -1,11 +1,10 @@
 const sweater_id = $('.sweater').data('sweater');
-console.log(sweater_id);
-$.ajax({
-	type: 'GET',
-	url: 'http://localhost:8000/sweaters/' + sweater_id,
+if (sweater_id) {
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8000/sweaters/' + sweater_id,
+  }).done(function(result) {
+    console.log(result);
+  })
+}
 
-}).done(function(result) {
-  console.log(result);
-}).fail(function(error) {
-	console.log('err', error);
-});
